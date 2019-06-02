e6ca8c3c4193d70f55b3cf94ec0ebba49473bec9
donghashin <shindh159@gmail.com>
2019-06-02 20:00:58 +0900

    [3.103p~110p] 트리형태 표현식

---
+++ src/Function/CollectionTest.kt
@@ -0,0 +1,15 @@
+package Function
+
+fun main(args: Array<String>) {
+    val set = hashSetOf(1, 7, 53)
+    val list = arrayListOf(1, 7, 53)
+    val map = hashMapOf(1 to "one", 7 to "seven", 53 to "fifty-three")
+    println(set.javaClass);
+    println(list.javaClass);
+    println(map.javaClass);
+
+    val strings = listOf("first", "second", "forteenth")
+    println(strings.last())
+    val numbers = setOf("1, 14, 2")
+    println(numbers.max())
+}
---
+++ src/Function/FunctionTest.kt
@@ -0,0 +1,27 @@
+package Function
+
+import java.lang.StringBuilder
+
+fun main(args: Array<String>) {
+    val list = listOf(1, 2, 3)
+    println(list)
+    // To add parameter naming to increase Readability
+    println(joinToString(list, separator = ";", prefix = "(", postfix = ")"))
+    println(joinToString(list, prefix = "# "))
+//    println(joinToString(list, ";", "(", ")"))
+}
+
+fun <T> joinToString(
+        collection: Collection<T>,
+        separator: String = ", ",
+        prefix: String = "",
+        postfix: String = ""
+    ): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
