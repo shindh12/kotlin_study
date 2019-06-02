f979dcfada358e790419e86c0cbdd474d083809b
donghashin <shindh159@gmail.com>
2019-06-02 19:28:34 +0900

    [2.84p] 트리형태 표현식

---
+++ src/Basic/Expr.kt
@@ -0,0 +1,6 @@
+package Basic
+
+interface Expr {
+    class Num(val value: Int) : Expr
+    class Sum(val left:Expr, val right:Expr) : Expr
+}
---
+++ src/Basic/TreeExp.kt
@@ -0,0 +1,16 @@
+import Basic.Expr
+import Basic.Expr.*
+
+fun main(args: Array<String>){
+    println(eval(Sum(Sum(Num(1), Num(2)), (Num(4)))))
+}
+fun eval(e: Expr) : Int {
+    if(e is Num) {
+        val n = e as Num
+        return n.value
+    }
+    if(e is Sum) {
+        return eval(e.right) + eval(e.left)
+    }
+    throw IllegalArgumentException("Unknown expression")
+}
